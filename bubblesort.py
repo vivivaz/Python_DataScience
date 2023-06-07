5943fe1d5b1c972400dd3fe244cb9d0f8e2fae5b
vivivaz <vivianleocardovaz@gmail.com>
2023-06-07 00:40:02 -0300

    implementando Bubble Sort

---
+++ bubblesort.py
@@ -0,0 +1,18 @@
+from random import shuffle
+
+lista = list(range(10))
+shuffle(lista)
+print(lista)
+
+def Bubble_Sort(lista):
+    a = len(lista) -1
+    while a != 0:
+        for i in range(a):
+            if lista[i] > lista[i+1]:
+                x = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = x
+        a -= 1
+    print(lista)
+
+Bubble_Sort(lista)
